87da97a46b1aa32dde0aea4d5143f1fb199f638b
焦糖甜心曲奇 <lmmlobb@gmail.com>
2021-05-18 21:54:39 +0800

    完成工厂布局，接下的是种群生成、迭代、挑选

--- genetic.py
+++ genetic.py
@@ -15,36 +15,30 @@ def ran_process(length):
 
 # 根据设备序列生成设备位置
 def layout(pro_series):
-    front_x = 0
-    front_y = 0
     for i in range(0, len(pro_series)-1):
         numPro = pro_series[i]  # 工序的编号
         # 设定X轴
         # print("前", sF.coord[numPro][0])
-        if front_x == 0:
+        if i == 0:
             sF.coord[numPro][0] = sF.area[numPro][0] / 2 + safe_distance
-            front_x = sF.coord[numPro][0]
         else:
             # 判断换行
-            testMaxX = sF.area[numPro][0] + sF.coord[pro_series[i-1]
-                                                     ][0] + sF.area[pro_series[i - 1]][0] / 2
-            if (testMaxX > sF.fX):
+            rightMaxX = sF.area[numPro][0] + sF.coord[pro_series[i - 1]][0] + \
+                sF.area[pro_series[i - 1]][0] / 2 + safe_distance # 最右边的X坐标 rightMaxX = 当前长+上一个坐标+上一个长/2+安全距离
+            if (rightMaxX > sF.fX):
                 print("换行")
-                sF.coord[numPro][0] = sF.area[numPro][0] / \
-                    2 + safe_distance + sF.coord[pro_series[i-1]][0]
-                front_x = 0
+                sF.coord[numPro][0] = sF.area[numPro][0] / 2 + safe_distance
             else:
                 sF.coord[numPro][0] = (sF.area[pro_series[i - 1]][0]+sF.area[numPro][0])/2 + \
                     sF.coord[pro_series[i - 1]][0] + \
                     safe_distance  # 当前X坐标 = (上一个长+当前长)/2+上一个坐标+安全距离
-                front_x = sF.coord[numPro][0]
 
         # 设定Y轴
         # sF.coord[numPro][1] = 0
-        if i == 0:
+        if i < 1:
             sF.coord[numPro][1] = sF.area[numPro][1] / 2
         else:
-            for j in range(0, i - 1):
+            for j in range(0, i):
                 # 判断x轴是否有交集，如果有，numPro的Y轴 = j的坐标+(numpro宽 + j宽)/2
                 leftMax = max(
                     sF.coord[pro_series[j]][0] - sF.area[pro_series[j]][0] / 2,
@@ -53,20 +47,18 @@ def layout(pro_series):
                     sF.coord[pro_series[j]][0] + sF.area[pro_series[j]][0] / 2,
                     sF.coord[numPro][0] + sF.area[numPro][0]/2)
 
-                if (leftMax > rightMin):  # 如果有交集
+                if (leftMax < rightMin):  # 如果有交集
                     # 应改写成当前坐标= 交集Y坐标+交集宽/2+当前Y/2+安全距离，缓存xtmpMaxCoordY，选择较高的tmpMaxCoordY
                     # 不用缓存，可以直接判断，如果大于，就更新当前Y坐标，如果否，就不更新
                     # sF.coord[numPro][1] = sF.coord[pro_series[j]][1] + (
                     #     sF.area[pro_series[j]][1] + sF.area[numPro][1]) / 2 + safe_distance
-                    if sF.coord[numPro][1] < sF.coord[pro_series[j-1]][1]+sF.area[pro_series[j-1]][1]/2+sF.area[numPro][1]+safe_distance:
-                        sF.coord[numPro][1] = sF.coord[pro_series[j-1]][1] + \
-                            sF.area[pro_series[j-1]][1]/2 + \
-                            sF.area[numPro][1]+safe_distance
+                    if sF.coord[numPro][1] < sF.coord[pro_series[j]][1]+(sF.area[pro_series[j]][1]+sF.area[numPro][1])/2+safe_distance:
+                        sF.coord[numPro][1] = sF.coord[pro_series[j]][1] + \
+                            (sF.area[pro_series[j]][1] +
+                             sF.area[numPro][1])/2+safe_distance
                 else:
-                    sF.coord[numPro][1] = sF.area[numPro][1] / \
-                        2 + safe_distance
-
-                # 选择较高的Y坐标
+                    sF.coord[numPro][1] = max( sF.coord[numPro][1],sF.area[numPro][1] / \
+                        2 + safe_distance)
 
         test.add_rectangle(sF.area[numPro], sF.coord[numPro])
 
